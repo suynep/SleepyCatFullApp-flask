01353993d0061af16138bf05b65aa7225829490a
suy <suy.nepal@hehe>
2024-12-26 18:32:32 +0545

    Deploy Buffs 🚃

--- app/ai.py
+++ app/ai.py
@@ -1,4 +1,23 @@
 import nltk
+import os
+
+# Define the path for nltk_data
+nltk_data_path = os.path.expanduser("~/nltk_data")
+
+# Check if the nltk_data directory exists
+if not os.path.exists(nltk_data_path):
+    print(f"Directory {nltk_data_path} does not exist. Creating and downloading data...")
+    nltk.download('vader_lexicon', download_dir=nltk_data_path)
+else:
+    # Check if vader_lexicon is specifically downloaded
+    try:
+        nltk.data.find('sentiment/vader_lexicon.zip')
+        print("VADER lexicon already downloaded.")
+    except LookupError:
+        print("Downloading VADER lexicon...")
+        nltk.download('vader_lexicon', download_dir=nltk_data_path)
+
+
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
 
